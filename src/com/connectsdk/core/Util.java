b5a826a5b24c8cca4a4c702e61a8a5e923d479f7
mrjorgexavier <mr.jorge.xavier@gmail.com>
2019-08-06 16:11:40 -0300

    reduzindo pra 1 a pool de threads

--- src/com/connectsdk/core/Util.java
+++ src/com/connectsdk/core/Util.java
@@ -45,7 +45,7 @@ public final class Util {
 
     static private Handler handler;
 
-    static private final int NUM_OF_THREADS = 2;
+    static private final int NUM_OF_THREADS = 1;
 
     static private Executor executor;
 
